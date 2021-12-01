c9eecf0c76792404846b9771b48099dade76d973
lucas-kok <56938053+lucas-kok@users.noreply.github.com>
2021-12-01 15:02:17 +0100

    Gabamaker 2021

--- CodeCademy/src/main/java/com/pekict/test/TestMain.java
+++ CodeCademy/src/main/java/com/pekict/test/TestMain.java
@@ -1,11 +1,21 @@
 
 package com.pekict.test;
 
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
 
 public class TestMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello bitchaass");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gabagoo Maker 2021:");
+        
+        while (true) {
+            System.out.print("Word:");
+            String word = scanner.nextLine();
+            System.out.println("Gaba" + word);
+        }
+
     }
 }
