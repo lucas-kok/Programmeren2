934252cb96d92cedd5d920024b2aa9d824cb6887
Lucas Kok <56938053+lucas-kok@users.noreply.github.com>
2022-01-11 19:02:22 +0100

    Resolved Package Errors

--- CodeCademy/src/main/java/com/codecademy/informationhandling/Student/StudentRepository.java
+++ CodeCademy/src/main/java/com/codecademy/informationhandling/Student/StudentRepository.java
@@ -4,6 +4,7 @@ import DatabaseConnection.DatabaseConnection;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.HashMap;
 
 public class StudentRepository {
@@ -13,14 +14,14 @@ public class StudentRepository {
     public StudentRepository() {
     }
 
-    public void createStudent(Student.Student student) {
-        //maak 1 letter van gender
+    public void createStudent(Student student) {
+        // Maak 1 letter van gender
         String query = "INSERT INTO Student VALUES ('" + student.getEmail() + "', '" + student.getName() + "', convert(datetime, '" + student.getBirthday().toString().replaceAll("-", "/") + "', 103)" +
                 ", '" + student.getGender() + "', '" + student.getAddress() + "', '" + student.getCity() + "', '" + student.getCountry() + "')";
         dbCon.setQuery(query);
     }
 
-    public HashMap<String, Student.Student> getAllStudents() throws SQLException {
+    public HashMap<String, Student> getAllStudents() throws SQLException {
         //Maak een woord van gender
         HashMap<String, Student> studentList = new HashMap<>();
         String query = "SELECT * FROM Student";
