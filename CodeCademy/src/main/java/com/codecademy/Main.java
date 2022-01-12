bb88e21fa1785e4f1aaef39da8da8954d8a072b9
TurboSjaantje <daanvdm@hotmail.com>
2022-01-12 15:29:53 +0100

    Added the correct package for the statistics page

--- CodeCademy/src/main/java/com/codecademy/Main.java
+++ CodeCademy/src/main/java/com/codecademy/Main.java
@@ -6,9 +6,6 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
         GUI gui = new GUI();
         Application.launch(GUI.class);
     }
---
+++ CodeCademy/src/main/java/com/codecademy/informationhandling/Statistics/StatisticsRepository.java
@@ -0,0 +1,4 @@
+package com.codecademy.informationhandling.Statistics;
+
+public class StatisticsRepository {
+}
