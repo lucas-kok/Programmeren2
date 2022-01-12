308bf94496195ce9f90fef4c758cce110871260d
TurboSjaantje <daanvdm@hotmail.com>
2022-01-12 12:16:12 +0100

    Started adding CRUD for Registration

--- CodeCademy/src/main/java/com/codecademy/informationhandling/ContentItem/ContentItemRepository.java
+++ CodeCademy/src/main/java/com/codecademy/informationhandling/ContentItem/ContentItemRepository.java
@@ -1,14 +1,11 @@
 package com.codecademy.informationhandling.ContentItem;
 
 import com.codecademy.informationhandling.Course.Course;
+import com.codecademy.informationhandling.Registration.Registration;
 
 import java.util.HashMap;
 
 public class ContentItemRepository {
-
-    public HashMap<String,ContentItem> getContentItems(Course course) {
-        return new HashMap<>();
-    }
 
     public void addContentItemToCourse(Course course, ContentItem contentItem) {
 
