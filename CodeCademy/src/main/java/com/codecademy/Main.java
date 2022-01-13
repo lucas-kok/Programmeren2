0cf2af19583cef9503eaead59493ffafa65d7127
TurboSjaantje <daanvdm@hotmail.com>
2022-01-13 19:05:44 +0100

    Ran GUI in main

--- CodeCademy/src/main/java/com/codecademy/Main.java
+++ CodeCademy/src/main/java/com/codecademy/Main.java
@@ -1,18 +1,13 @@
 package com.codecademy;
 
-import com.codecademy.informationhandling.ContentItem.ContentItem;
-import com.codecademy.informationhandling.ContentItem.ContentItemRepository;
-import com.codecademy.informationhandling.Course.Course;
-import com.codecademy.informationhandling.Course.CourseRepository;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
+import com.codecademy.gui.GUI;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
-//        GUI gui = new GUI();
-//        Application.launch(GUI.class);
+        GUI gui = new GUI();
+        Application.launch(GUI.class);
     }
 
 }
